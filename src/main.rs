ca66cd84d3b556711f00d25aeae31bf90d111161
Timtangxi <3237125374@qq.com>
2024-06-05 18:47:39 +0800

    updata

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-// fn fun(b: i32 , a: i32)-> i32
-// {
-//     return a+b;
-// }
-// fn name() -> i32 {
-//      1
-// }
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-        enum Book {
-            Papery(u32),
-            Electronic(String)
+
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        let f = io::stdin().read_line(&mut guess);
+        if let Err(_f) = f {
+            println!("Failed to read line");
         }
-        let book: Book = Book::Electronic(String::from("url"));
-        if let Book::Papery(index) = book {
-            println!("Papery {}", index);
-        } else {
-            println!("Not papery book");
+
+        // let guess: u32 = guess.trim().parse().expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed: {guess}, But too small!"),
+            Ordering::Greater => println!("You guessed: {guess}, But too big!"),
+            Ordering::Equal => {
+                println!("You guessed: {guess}, You win!");
+                break;
+            }
         }
     }
-// fn main() {
-//     let a: &str = "uoad";
-//     let mut test : i32 = 5; 
-//     test = 6;
-//     let x: i32 =  5;
-//     let x: i32 = x + 1;
-//     println!("{} {} {} {} {}", a, test, x, fun(5, 6), name());
-// }
-
-/* 
-fn main() { 
-    let a = [10, 20, 30, 40, 50]; 
-    for i in a.iter() { 
-    println!("值为 : {}", i); 
-    } 
 }
-*/
